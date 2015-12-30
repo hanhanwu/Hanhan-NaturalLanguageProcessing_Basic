9346b738671e4d03b632a2b310a3be54d50dbf65
hanhan <wuhanhan999@gmail.com>
2015-12-29 22:58:04 -0800

    create tf_idf_term_search.py

---
+++ tf_idf_term_search.py
@@ -0,0 +1,37 @@
+'''
+Created on Dec 21, 2015
+@author: hanhanwu
+nltk provides tf-idf itself
+Note: in practical, tf-idf is a method to narrow down corpus based on search terms
+'''
+import nltk
+import json
+
+def main():
+    f_path = '/Users/hanhanwu/Documents/workspace/PythonLearning/MiningGooglePlus/107033731246200681024.json'
+    data = json.loads(open(f_path).read())
+    
+    QUERY_TERMS = ['mobile']   # You can change the search terms here
+    
+    activities = [activity['object']['content'].lower().split() for activity in data if activity['object']['content'] != '']
+    
+    # nltk TextCollection has tf-idf itself
+    tc = nltk.TextCollection(activities)
+    
+    relevant_activities = []
+    
+    for i in range(len(activities)):
+        score = 0
+        for term in QUERY_TERMS:
+            score += tc.tf_idf(term.lower(), activities[i])
+        if score > 0:
+            relevant_activities.append({'score': score, 'title': data[i]['title'], 'url': data[i]['url']}) 
+        
+    relevant_activities = sorted(relevant_activities, key = lambda a: a['score'], reverse = True)
+    for ra in relevant_activities:
+        print 'title: ', ra['title']
+        print 'url: ', ra['url']
+        print 'score: ', ra['score']
+    
+if __name__ == '__main__':
+    main()
